c53478f58a54deb3ff0b02805590a1dc7f4eef05
grace <grace.zhao@farmingdale.edu>
2023-01-23 18:29:18 -0500

    Initial version

---
+++ recursiveFunctions/myQuickSort.py
@@ -0,0 +1,27 @@
+'''
+Code description: A (pure) recursive implementation of Quick Sort.
+Author: gz
+Date: 1/23/23
+'''
+
+from typing import List, Any
+
+def myQuickSort(mylist: int) -> int:
+    pivot = mylist[0]
+    left: list[Any] = [x for x in mylist if x < pivot]
+    right: list[Any] = [y for y in mylist if y > pivot]
+    if len(left) > 1:
+        left = myQuickSort(left)
+    if len(right) > 1:
+        right = myQuickSort(right)
+
+    return left + [pivot] + right
+
+
+# Test code
+if __name__ == '__main__':
+    mylist = [1, 11, 4, 6, 8, 2]
+    alist = myQuickSort(mylist)
+    print(f'Here you go: {alist}')
+
+
